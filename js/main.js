ade7490095edaba35c1d6efa8d74ed4aa96779d5
Your Name <you@example.com>
2022-01-12 19:22:43 +1100

    new comit

--- js/main.js
+++ js/main.js
@@ -15,7 +15,8 @@ $(document).ready(function () {
             $("#3").html() === "x" && $("#6").html() === "x" && $("#9").html() === "x" ||
             $("#1").html() === "x" && $("#5").html() === "x" && $("#9").html() === "x" ||
             $("#3").html() === "x" && $("#5").html() === "x" && $("#7").html() === "x") {
-            alert("The winner is: X");
+            $('.gif').css('visibility', 'visible')
+            
             console.log('the winner is x');
         } else if (
             $("#1").html() === "o" && $("#2").html() === "o" && $("#3").html() === "o" ||
@@ -26,39 +27,48 @@ $(document).ready(function () {
             $("#3").html() === "o" && $("#6").html() === "o" && $("#9").html() === "o" ||
             $("#1").html() === "o" && $("#5").html() === "o" && $("#9").html() === "o" ||
             $("#3").html() === "o" && $("#5").html() === "o" && $("#7").html() === "o") {
-            alert("The winner is: O");
+            $('.gif').css('visibility', 'visible')
+            // alert("The winner is: O");
         }
     }
-    
-     //on click function for all <div class="box"> 9 boxes
-    
-    $('.box').on('click', function(){ 
-        
-       
+
+    //on click function for all <div class="box"> 9 boxes
+
+    $('.box').on('click', function () {
+
+
         // first check if the box is empty. 
-        if( $(this).html() === 'x' || $(this).html() === 'o' ){ // check if x is placed
-           
+        if ($(this).html() === 'x' || $(this).html() === 'o') { // check if x is placed or o played
+
             return // this is to stop the function 
-       
+
         }
-        if (playerTurn === true){         
-            
+        if (playerTurn === true) {
+
             $(this).html('x'); // set the html in the clicked element to 'x'
             playerTurn = false; // playerTurn is over now, so set playerTurn to false.
-            
+
             winning()
-        
-        } else { 
-            $(this).html('o'); 
+
+        } else {
+            $(this).html('o');
             playerTurn = true; // its going to be player turn next click.
             winning()
+            const clickSound = new Audio('mixkit-shaker-bell-alert-599.mp3')
+
+            //in your .box on click function
+            clickSound.play()
         }
- // end of on click function
-    $( '#button' ).on('click',function() {
-        alert( "Handler for .click() button." );
-      })
-       
-           
-     
+        // end of on click function
+        $('#popup').on('click', function () {
+            $('.gif').css('visibility', 'hidden')
+            $('.box').html('')
+            // end of on click function
+            
+        })
+        
+
+
+
     })
 });//end of document.ready function
